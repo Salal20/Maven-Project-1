13883f784be02705b3c5f4c1e74272d05117e2c4
Salal20 <rubinaburiro@yahoo.com>
2022-12-09 19:35:37 -0500

    first commit

---
+++ src/test/java/mavev_project_1/Automation_Test.java
@@ -0,0 +1,21 @@
+package mavev_project_1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Automation_Test {
+
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://google.com");
+		
+		
+		
+
+	}
+
+}
